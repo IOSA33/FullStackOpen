62f585d15945f95c84fb763ff282416e5d250bd9
IOSA33 <sedinkin33@gmail.com>
2026-03-15 15:24:47 +0200

    2.8 puhelinluettelo

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -4,7 +4,11 @@ const ShowList = ({ allPersons }) => {
   return (
     <div>
       {allPersons.map(person => {
-        return <p key={person.name}>{person.name}</p>
+        return ( 
+          <p key={person.name}> 
+            {person.name} {person.number}
+          </p>
+        )
       })}
     </div>
   )
@@ -12,28 +16,36 @@ const ShowList = ({ allPersons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setnewPhoneNumber] = useState('')
 
   const addNote = (event) => {
     event.preventDefault()
 
     if (persons.filter(checkPerson => checkPerson.name === newName).length === 0 ) {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newPhoneNumber
       }
-
       setPersons(persons.concat(personObject))
     } else {
       alert(`${newName} is already added to phonebook`)
     }
 
     setNewName('')
+    setnewPhoneNumber('')
   }
 
-  const handleNoteChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handlePhoneChange = (event) => {
+    setnewPhoneNumber(event.target.value)
   }
 
   return (
@@ -43,7 +55,12 @@ const App = () => {
         
         <div> 
           name: 
-          <input value={newName} onChange={handleNoteChange}/>   
+          <input value={newName} onChange={handleNameChange}/>   
+        </div>
+
+        <div>
+          number: 
+          <input value={newPhoneNumber} onChange={handlePhoneChange}/>
         </div>
 
         <div> 
