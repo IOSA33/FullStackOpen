62008b2142b1744de3f4997c205bbb5e18992dbb
IOSA33 <sedinkin33@gmail.com>
2026-06-13 16:20:41 +0300

    3.15 - 3.18* added async

--- Osa3/puhelinluetteloFull/backend/index.js
+++ Osa3/puhelinluetteloFull/backend/index.js
@@ -26,23 +26,59 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            next(error)
+        })
 })
 
-app.get('/info', (request, response) => {
+const getData = async () => {
+    const DB = await Person.find({})
+    return DB.length
+}
+
+app.get('/info', async (request, response) => {
     const timestamp = new Date()
-    response.send('<p>Phonebook has info for '+persons.length+' people</p> <p>'+timestamp.toString()+'</p>')
+
+    const length = await getData()
+
+    response.send('<p>Phonebook has info for '+length+' people</p> <p>'+timestamp.toString()+'</p>')
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findById(request.params.id)
+        .then(result => {
+            if (!result) {
+                response.status(404).end()
+            }
+
+            result.name = name
+            result.number = number
+
+            return result.save().then((updatedPerson) => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+}) 
 
 const generateId = () => {
   const maxId = persons.length > 0
@@ -69,6 +105,26 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
