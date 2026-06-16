701a062d2fbfca88e384112dc6e9b041ff3771be
IOSA33 <sedinkin33@gmail.com>
2026-06-16 22:25:46 +0300

    4.2. bloglista step2

--- Osa4/bloglist/index.js
+++ Osa4/bloglist/index.js
@@ -1,39 +1,10 @@
 const dns = require("node:dns");
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
