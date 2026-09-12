f3ec397fbeff3b0e2e80550ae33d8f0707ae18a8
IOSA33 <sedinkin33@gmail.com>
2026-09-12 16:40:09 +0300

    7.5 done

--- Osa7/routed-anecdotes/src/hooks/index.js
+++ Osa7/routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import anecdoteService from '../services/anecdotes'
 
 export const useField = (type) => {
     const [value, setValue] = useState('')
@@ -15,5 +16,23 @@ export const useField = (type) => {
         type,
         value,
         onChange
+    }
+}
+
+export const useAnecdotes = () => {
+    const [anecdotes, setAnecdotes] = useState([])
+
+    useEffect(() => {
+        anecdoteService.getAll().then(data => setAnecdotes(data))
+    }, [])
+
+    const addAnecdote = async (object) => {
+        const response_json = await anecdoteService.createNew(object)
+        setAnecdotes(anecdotes.concat(response_json))
+    }
+
+    return {
+        anecdotes,
+        addAnecdote
     }
 }
